87e8fbe7022cb34ac7bf2e9daea15558ecca4b6d
Marcus Vinícius Prata Marinho <marcus.marinho@spread.com.br>
2018-08-31 12:37:35 -0300

    Angular Animation

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { style, transition, animate, state, trigger } from '@angular/animations';
+import { Imagem } from './imagem.model'
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  // Angular Animations
+  animations:[
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity: 1
+      })),
+      transition('escondido <=> visivel' , animate('3s ease-in'))
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
 
   constructor() { }
 
+  public estado: string = 'escondido'
+
+  public imagens: Imagem[] = [
+    { estado: 'visivel', url: '/assets/banner-acesso/img_1.png' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_2.png' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_3.png' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_4.png' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_5.png' }
+  ]
+
   ngOnInit() {
+    setTimeout(() => this.logicaRotacao(), 3300 )
+  }
+
+  public logicaRotacao(): void {
+    
+    //auxilia na exibicao da imagem seguinte
+    let idx: number = 0
+
+    //ocultar imagem
+    for ( let i : number = 0 ; i <= 4 ; i++ ) {
+
+        if (this.imagens[i].estado === 'visivel') {
+          this.imagens[i].estado = 'escondido'
+          idx = i === 4 ? 0 : i + 1
+          break
+        }
+    }
+    //exibir a proxima imagem
+    this.imagens[idx].estado = 'visivel'
+    setTimeout(() => this.logicaRotacao(), 3300 )
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
@@ -18,7 +19,8 @@ import { CadastroComponent } from './acesso/cadastro/cadastro.component';
     CadastroComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
