964451934974c27fe2d6a37d4a7b2662ea7dd4af
Marcus Vinícius Prata Marinho <marcus.marinho@spread.com.br>
2018-08-31 16:29:39 -0300

    more animations

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,11 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { transition } from '@angular/animations';
+import { trigger } from '@angular/animations';
+import { state } from '@angular/animations';
+import { style } from '@angular/animations';
+import { animate } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.css']
+  styleUrls: ['./acesso.component.css'],
+  animations : [
+    trigger('animacao-banner', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(-50px, 0)'}),
+        animate('500ms 0s ease-in-out')
+      ])
+    ]),
+    trigger('animacao-painel', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado' , [
+        style({ opacity: 0, transform: 'translate(50px, 0)'}),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])   
+  ]
 })
+
 export class AcessoComponent implements OnInit {
+
+  public estadoBanner: string ='criado'
+  public estadoPainel: string ='criado'
 
   constructor() { }
 
