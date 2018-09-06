4cf2939c9f50266120517c08fea529dd80a61559
Marcus Vinícius Prata Marinho <marcus.marinho@spread.com.br>
2018-09-06 16:15:14 -0300

    home bug fix

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -55,6 +55,10 @@ export class Autenticacao {
         if (this.token_id === undefined && localStorage.getItem('idToken') != null) {
             this.token_id = localStorage.getItem('idToken')
         }
+
+        if (this.token_id === undefined) {
+            this.router.navigate(['/'])
+        } 
        
         return this.token_id !== undefined 
     }
